38cf36bf5a523b440c2824739b5766b8b9fcbad8
switchkiller <emailbarung@gmail.com>
2016-04-03 12:27:02 +0530

    Recaptcha is not running on localhost

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
         <title>Sign up</title>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+        <style>
+        .g-recaptcha {
+            margin-bottom: 10px;
+        }
+        </style>
     </head>
     <body>
         <div class="container">
@@ -17,11 +23,13 @@
                             <label for="email">Email address</label>
                             <input type="email" name="email" class="form-control" id="email" placeholder="you@domain.com">
                         </div>
+                        <div class="g-recaptcha" data-sitekey="6LfFYxwTAAAAAPkND0Jpz6wfR-VUzjx00VjCdb0w"></div>
 
                         <input type="submit" value="Sign up" class="btn btn-primary">
                     </form>
                 </div>
             </div>
         </div>
+        <script src="https://www.google.com/recaptcha/api.js"></script>
     </body>
 </html>
---
+++ signup.php
@@ -0,0 +1,21 @@
+<?php
+
+$email = $_POST['email'];
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+	CURLOPT_RETURNTRANSFER => 1,
+	CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
+	CURLOPT_POST => 1,
+	CURLOPT_POSTFIELDS => [
+		'secret' => '6LfFYxwTAAAAALkP7btrWNo61nPl4Ia1-3nBN2vK',
+		'response' => $_POST['g-recaptcha-response'],
+
+	]
+]);
+
+$response = json_decode(curl_exec($curl));
+
+var_dump($response);
